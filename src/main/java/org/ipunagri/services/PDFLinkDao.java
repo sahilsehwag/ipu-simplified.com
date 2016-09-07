94a702278e91339b9347720c2ad380c28a693462
Jaat <sehwagsahil002@gmail.com>
2016-09-07 18:42:42 +0530

    Designed PDFLinkExtractorTask, Added TaskSchedular Configuration && Redesigned PDFLinkDAO Service

--- src/main/java/org/ipunagri/services/PDFLinkDao.java
+++ src/main/java/org/ipunagri/services/PDFLinkDao.java
@@ -2,19 +2,30 @@ package org.ipunagri.services;
 
 
 import org.ipunagri.models.PDFLink;
+import org.ipunagri.support.models.ParsedRow;
 
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import java.util.List;
+import java.util.Map;
 
 
 @NamedQueries({
         @NamedQuery(name="getRowsByPDFType", query="FROM PDFLink AS pdf WHERE pdf.pdfType = :pdfType"),
+        @NamedQuery(name="getRowsByDateRange", query="FROM PDFLink AS pdf WHERE (pdf.uploadDate >= :startDate and pdf.uploadDate <= :endDate)"),
 })
 public class PDFLinkDao extends Dao{
 
     public List<PDFLink> getRows(String pdfType) {
         return (List<PDFLink>)hibernateTemplate.findByNamedQuery("getRowsByPDFType", pdfType);
+    }
+
+    public List<PDFLink> getRowsByDateRange(String startDate, String endDate){
+        return (List<PDFLink>)hibernateTemplate.findByNamedQuery("getRowsByDateRange", startDate, endDate);
+    }
+
+    public void saveOrUpdateAll(List<PDFLink> rows){
+
     }
 
 }
---
+++ src/main/java/org/ipunagri/support/tasks/PDFLinkExtractionTask.java
@@ -0,0 +1,24 @@
+package org.ipunagri.support.tasks;
+
+
+import org.ipunagri.services.PDFLinkDao;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class PDFLinkExtractionTask {
+
+    @Autowired
+    PDFLinkDao pdfLinksDAO;
+
+    public void getResultPDFLinks(){
+
+    }
+
+    public void getNoticePDFLinks(){
+
+    }
+
+    public void getDatesheetPDFLinks(){
+
+    }
+
+}
